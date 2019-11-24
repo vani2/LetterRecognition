57ffa7bae7be07d894573764c324da5f6996803b
Ivan Vavilov <iv@redmadrobot.com>
2019-11-24 19:52:34 +0300

    Initial Commit

---
+++ LetterComposer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetterComposer
+//
+//  Created by Ivan Vavilov on 24.11.2019.
+//  Copyright © 2019 Ivan Vavilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
